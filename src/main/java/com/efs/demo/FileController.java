146f79f34caee1b6c988441271b43c44c656b2ff
SaiManideep00 <saimanideep00@gmail.com>
2025-07-11 15:39:44 -0400

    Initial commit

---
+++ src/main/java/com/efs/demo/FileController.java
@@ -0,0 +1,31 @@
+package com.efs.demo;
+
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+public class FileController {
+    private static final String EFS_PATH = "/mnt/efs";
+    @PostMapping(value="/upload", consumes =  MediaType.MULTIPART_FORM_DATA_VALUE)
+    public String uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
+        String filePath = EFS_PATH + "/" + file.getOriginalFilename();
+        Files.write(Paths.get(filePath), file.getBytes());
+
+        return "File uploaded successfully to EFS: " + file.getOriginalFilename();
+    }
+    @GetMapping("/files")
+    public List<String> listFiles() throws IOException {
+        // Stream the list of file names in the EFS directory
+        return Files.list(Paths.get(EFS_PATH))
+                .map(path -> path.getFileName().toString())
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/test/java/com/efs/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.efs.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
